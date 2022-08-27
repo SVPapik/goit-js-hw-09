b5416ff912819f85cd7dab5c494b45532b8a7b7d
Vitaliy <fireflier87@gmail.com>
2022-08-27 16:12:37 +0300

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const START = document.querySelector('button[data-start]');
+const STOP = document.querySelector('button[data-stop]');
+const BODY = document.querySelector('body');
+
+let timerId = null;
+
+STOP.disabled = true;
+
+START.addEventListener('click', () => {
+  START.disabled = true;
+  STOP.disabled = false;
+
+  timerId = setInterval(() => {
+    BODY.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+STOP.addEventListener('click', () => {
+  clearInterval(timerId);
+  START.disabled = false;
+  STOP.disabled = true;
+});
